b112610ba55dafd2c608b97c3a69a2776d917761
Jon Krazov <Krazov@users.noreply.github.com>
2016-06-13 10:50:49 +0200

    Update cheatsheet.js

--- cheatsheet.js
+++ cheatsheet.js
@@ -1,2 +1,9 @@
 // remove elements from page
 Array.prototype.forEach.call(document.querySelectorAll('.className'), function (element) { element.parentNode.removeChild(element); });
+
+// get duration of transition
+[].forEach.call(document.querySelectorAll('.elements'), function (element) {
+    console.log(Number(window.getComputedStyle(element).getPropertyValue('transition').split(/\s/)[1].replace(/sm?/g, '')));
+});
+
+console.log(Number(window.getComputedStyle(document.querySelector('.element')).getPropertyValue('transition').split(/\s/)[1].replace(/sm?/g, '')));
